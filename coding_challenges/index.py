bea661cedfba05970606544a117bc73331a258b3
Zachary Boelter <zboelter@gmail.com>
2023-06-08 16:39:04 -0700

    refactor summation to 1 line

--- coding_challenges/index.py
+++ coding_challenges/index.py
@@ -21,12 +21,18 @@ def is_square(n):
 
 
 
+# def summation(num):
+#     sum = 0
+#     for i in range(1, num + 1):
+#         sum += i
+#     return sum
+
+
+
 def summation(num):
-    sum = 0
-    for i in range(1, num + 1):
-        sum += i
-    return sum
+    return sum(range(num + 1))
 
 print(summation(5))
 print(summation(966128))
 print(summation(-10))
+print(summation(10))
