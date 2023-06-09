fc65be4958ed26e68039de125b5577b8a242e710
Zachary Boelter <zboelter@gmail.com>
2023-06-09 09:18:06 -0700

    type

--- daily_work_30_days/day_2/variables.py
+++ daily_work_30_days/day_2/variables.py
@@ -8,6 +8,14 @@ age = 33
 year = 2023
 is_married, is_true, is_light_on = False, True, True
 
+print(type(first_name))
+print(type(last_name))
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+type(is_light_on)
 
-
-print(is_light_on)
