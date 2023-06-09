e0e41fabd7e6d1471fa0449ed6cd125d7d31967a
Zachary Boelter <zboelter@gmail.com>
2023-06-09 08:47:33 -0700

    row_sum_odd_numbers

--- coding_challenges/README.md
+++ coding_challenges/README.md
@@ -7,8 +7,11 @@ Function takes four parameters, one for each x and y point (2 dimentions). Take 
 Check if n is less than zero to remove negative intergers. 
 Get square root of n, check if n equals square ** 2.
 
-### 3. Summation
+### 3. Summation.
 Set sum to zero. For loop that starts at 1 and goes to num (parameter) + 1. Add each iteration to sum. Return.
 
-### 4. Regex Validate PIN Code
+### 4. Regex Validate PIN Code.
 Check if PIN is length of either 4 or 6. Check if PIN is digits. Return if true.
+
+### 5. Return sum of odd nums.
+Return n^3.
--- coding_challenges/index.py
+++ coding_challenges/index.py
@@ -41,8 +41,22 @@ def summation(num):
 def validate_pin(pin):
     return len(pin) in (4, 6) and pin.isdigit()
 
-print(validate_pin('3333'))
-print(validate_pin('444444'))
-print(validate_pin('123d'))
-print(validate_pin('123'))
-print(validate_pin('....'))
+
+
+# Given the triangle of consecutive odd numbers:
+
+#              1
+#           3     5
+#        7     9    11
+#    13    15    17    19
+# 21    23    25    27    29
+# ...
+
+# Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+
+def row_sum_odd_numbers(n):
+    return n ** 3
+
+
+
