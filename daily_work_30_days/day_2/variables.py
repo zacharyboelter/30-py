fbe318b0b7ccce06156038dc28669e1c3a204eba
Zachary Boelter <zboelter@gmail.com>
2023-06-09 09:24:35 -0700

    level 2

--- daily_work_30_days/day_2/variables.py
+++ daily_work_30_days/day_2/variables.py
@@ -8,8 +8,8 @@ age = 33
 year = 2023
 is_married, is_true, is_light_on = False, True, True
 
-print(type(first_name))
-print(type(last_name))
+# print(type(first_name))
+# print(type(last_name))
 type(full_name)
 type(country)
 type(city)
@@ -19,3 +19,19 @@ type(is_married)
 type(is_true)
 type(is_light_on)
 
+
+num_one = 5
+num_two = 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+r = 30
+area_of_circle = 3.14159 * (r ** 2)
+cir_of_circle = 2 * 3.14159 * r
+
+print(cir_of_circle)
