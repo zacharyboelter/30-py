a5331e06eae9a6a7bbee18291aa9f5625761cea1
Zachary Boelter <zboelter@gmail.com>
2023-06-09 09:16:28 -0700

    level 1

--- daily_work_30_days/day_2/variables.py
+++ daily_work_30_days/day_2/variables.py
@@ -3,6 +3,11 @@ first_name = 'Zachary '
 last_name = 'Boelter'
 full_name = first_name + last_name
 country = 'Mexico'
+city = 'CDMX'
+age = 33
+year = 2023
+is_married, is_true, is_light_on = False, True, True
 
 
-print(full_name)
+
+print(is_light_on)
