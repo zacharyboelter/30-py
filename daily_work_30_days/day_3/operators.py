6939a0fd9aa7efe51fbd4aae3e3407dd5b276707
Zachary Boelter <zboelter@gmail.com>
2023-06-15 15:18:20 -0700

    area of tri function w/ propmts for input

--- daily_work_30_days/day_3/operators.py
+++ daily_work_30_days/day_3/operators.py
@@ -3,3 +3,10 @@ hieght = 73.0
 
 my_complex_number = 2 + 3j
 
+def area():
+    base = float(input("Enter triangle base: "))
+    height = float(input("Enter triangle height: "))
+    total = (0.5 * base * height)
+    print(f"The area of your triangle is {total}")
+
+area()
