1aeb98db9cd82e404922121128611020598f3135
Zachary Boelter <zboelter@gmail.com>
2023-06-28 15:24:40 -0700

    nested loop to print dominoes w/o doubles

--- automate_google/google.py
+++ automate_google/google.py
@@ -1,5 +1,13 @@
-for x in range(5):
-    print(x)
+# for x in range(5):
+#     print(x)
 
-for i in range(50):
-    print(i)
+# for i in range(50):
+#     print(i)
+
+
+# nested loop to print every dominoe with no doubles
+
+for left in range(7):
+    for right in range(left, 7):
+        print(f'[{left} | {right}]', end=' ')
+    print()
