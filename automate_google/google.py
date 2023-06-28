d2282af9d003916a3271693a4a7e6aef21fa85d9
Zachary Boelter <zboelter@gmail.com>
2023-06-28 16:04:00 -0700

    factorial using recursion and boilerPlate for recursive function

--- automate_google/google.py
+++ automate_google/google.py
@@ -7,15 +7,28 @@
 
 # nested loop to print every dominoe with no doubles
 
-for left in range(7):
-    for right in range(left, 7):
-        print(f'[{left} | {right}]', end=' ')
-    print()
+# for left in range(7):
+#     for right in range(left, 7):
+#         print(f'[{left} | {right}]', end=' ')
+#     print()
 
 
-teams = ['pandas', 'dragons', 'rats', 'snakes', 'clippers', 'sharks']
+# teams = ['pandas', 'dragons', 'rats', 'snakes', 'clippers', 'sharks']
 
-for home_team in teams:
-    for away_team in teams:
-        if home_team != away_team:
-            print(f"Home: {home_team.capitalize()} vs Away: {away_team.capitalize()}")
+# for home_team in teams:
+#     for away_team in teams:
+#         if home_team != away_team:
+#             print(f"Home: {home_team.capitalize()} vs Away: {away_team.capitalize()}")
+
+def factorial(n):
+    if n < 2:
+        return 1
+    return n * factorial(n - 1)
+    
+print(factorial(10))
+
+
+# def recursive_function(parameters):
+#     if base_case_condition(parameters):
+#         return base_case_value
+#     recursive_function(modified_parameters)
