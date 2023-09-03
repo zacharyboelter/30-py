67aae72ebab58fdb6cec3893a18ecb19689d3792
Zachary Boelter <zboelter@gmail.com>
2023-09-02 17:06:00 -0700

    list comprehension practice

--- coding_challenges/index.py
+++ coding_challenges/index.py
@@ -162,3 +162,25 @@ for num in range(10):
 
 #list comprehension
 squares = [num ** 2 for num in range(10)]
+
+# Let's learn about list comprehensions! You are given three integers and representing the dimensions of a cuboid along with an integer . Print a list of all possible coordinates given by on a 3D grid where the sum of is not equal to . Here, . Please use list comprehensions rather than multiple loops, as a learning exercise. 
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+# list comprehension format
+coordinates = [[i, j, k] for i in range(x + 1) for j in range (y + 1) for k in range(z + 1) if i + j + k != n]
+
+
+x = int(input())
+y = int(input())
+z = int(input())
+n = int(input())
+#for loop format
+cubic_coordinates = []
+for i in range (x + 1):
+    for j in range (y + 1):
+        for k in range (z + 1):
+            if i + j + k != n:
+                cubic_coordinates.append([i, j, k])
